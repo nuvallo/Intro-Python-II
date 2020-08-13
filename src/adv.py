4bf1213aab9ec88ec9259dbb8ff63f1cef25dd8b
nuvallo <nuvallo.inc@gmail.com>
2020-08-13 16:26:16 -0400

    while loop created

--- src/adv.py
+++ src/adv.py
@@ -4,23 +4,23 @@ from player import Player
 # Declare all the rooms
 
 room = {
-        'outside':  Room("Outside Cave Entrance",
-            "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Cave Entrance",
+                     "North of you, the cave mount beckons"),
 
-        'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-        'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-        'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-        'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
-        }
+}
 
 
 # Link rooms together
@@ -42,7 +42,7 @@ room['treasure'].s_to = room['narrow']
 
 print('Welcome to my adventure game!')
 playerName = input('Please enter your name: ')
-p = Player(playerName, room['outside'])
+player = Player(playerName, room['outside'])
 
 # Write a loop that:
 #
@@ -54,3 +54,22 @@ p = Player(playerName, room['outside'])
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    currentRoom = player.room
+    movementChoice = ['n', 'e', 's', 'w']
+    print('\n')
+    userChoice = input(
+        'What do you want to do? You can Move n, e, s, or you can enter q to quit: ')
+
+    if userChoice in movementChoice:
+        if currentRoom.__dict__[f'{userChoice}_to'] == None:
+            print("There is nothing that direction!")
+        else:
+            player.room = currentRoom.__dict__[f'{userChoice}_to']
+
+    elif userChoice == "q":
+        print("Thanks for playing!")
+        exit()
+    else:
+        print("\nThat is not allowed movement input.")
